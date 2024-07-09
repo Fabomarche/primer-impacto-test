6dae6c576c24addada89555586f43b15b01102d7
fabomarche <fabriciomarche@hotmail.com>
2024-07-09 19:36:33 -0300

    more prettier config

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,20 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello()
+    }
 
-  // Endpoint to get video games
+    // Endpoint to get video games
 
-  // Endpoint to delete a video game
+    // Endpoint to delete a video game
 
-  // Endpoint to add a video game
+    // Endpoint to add a video game
 
-  // Endpoint to update a video game
+    // Endpoint to update a video game
 }
--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from '@nestjs/common';
-import AppRepository from './app.repository';
+import { Injectable } from '@nestjs/common'
+import AppRepository from './app.repository'
 
 @Injectable()
 export class AppService {
-  constructor(private readonly repo: AppRepository) {}
+    constructor(private readonly repo: AppRepository) {}
 
-  getHello(): string {
-    return 'Hello World!';
-  }
+    getHello(): string {
+        return 'Hello World!'
+    }
 
-  // Define methods to interact with the repository
+    // Define methods to interact with the repository
 }
