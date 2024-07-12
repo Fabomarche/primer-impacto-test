407f839bf09731c19fb12dfcb686ed1a76b2274c
fabomarche <fabriciomarche@hotmail.com>
2024-07-12 14:57:08 -0300

    Refactored context

--- front/src/context/context.ts
+++ front/src/context/context.ts
@@ -1,3 +1,12 @@
-import React from 'react';
+import { createContext, Dispatch, SetStateAction } from 'react';
+import { VideoGame } from '../services/videoGameService';
 
-export const VideoGameContext = React.createContext(null);
+interface VideoGameContextProps {
+    updatedVideoGame: VideoGame | null;
+    setUpdatedVideoGame: Dispatch<SetStateAction<VideoGame | null>>;
+}
+
+export const VideoGameContext = createContext<VideoGameContextProps>({
+    updatedVideoGame: null,
+    setUpdatedVideoGame: () => {},
+});
