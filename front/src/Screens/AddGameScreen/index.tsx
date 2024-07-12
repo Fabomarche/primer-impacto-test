dc1eee9547b4b5d491c5218d1da6f114726326c3
fabomarche <fabriciomarche@hotmail.com>
2024-07-12 14:58:56 -0300

    Refactored Add & update screens for FormGame

--- front/src/Screens/AddGameScreen/index.tsx
+++ front/src/Screens/AddGameScreen/index.tsx
@@ -1,8 +1,22 @@
 import { useAddVideoGame } from '../../services/videoGameService';
 import GameForm from '../../components/GameForm'
+import Loading from '../../components/Loading'
+import { CustomNotification } from '../../components/CustomNotification'
 
 const AddGameScreen = () => {
     const {mutateAsync: handleAdd, isLoading, error: addError } = useAddVideoGame()
+
+    if (isLoading) {
+        return <Loading />
+    }
+    
+    if (addError instanceof Error) {
+        CustomNotification({
+            type:'error',
+            message:'Error',
+            description:addError.message,
+        })
+    }
 
     return (
         <>
